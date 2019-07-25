61b91056ae96acf2af8410b8c5e8ae25e0635ee1
Brian George <brian@bhgeorge.com>
2019-07-25 17:01:41 -0500

    Added external link component.

---
+++ components/ExternalLink.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <a :href="link" target="_blank" rel="noopener">
+    <span>{{ text }}</span>
+    <span class="u-visually-hidden">(opens in a new tab)</span>
+    <Icon
+      type="newWindow"
+      :modifiers="['u-m-left-xxs', 'u-font-vert-mid', 'o-icon--s']"
+    />
+  </a>
+</template>
+
+<script>
+import Icon from '@/components/Icon';
+
+export default {
+  props: {
+    link: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    Icon,
+  },
+};
+</script>
